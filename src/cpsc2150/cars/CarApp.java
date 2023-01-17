189159fbd6eb423d5a592bd5d5763e62f1f32663
plantstevie <splant@clemson.edu>
2023-01-17 15:34:02 -0500

    Fixed data verification erros

--- src/cpsc2150/cars/CarApp.java
+++ src/cpsc2150/cars/CarApp.java
@@ -56,7 +56,7 @@ public class CarApp {
         LocalDate currentDate = LocalDate.now();
         int currentYear = currentDate.getYear();
         //add a breakpoint in the next line
-        while (year < 1900 && year > currentYear + 1) {
+        while (year < 1900 || year > currentYear + 1) {
             System.out.println("Year must be greater than or equal to 1900 and less than or equal to next year!");
             System.out.println("What is the year of the car");
             year = Integer.parseInt(scanner.nextLine());
